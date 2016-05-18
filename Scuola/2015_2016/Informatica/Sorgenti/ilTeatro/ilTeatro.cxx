2a5f5f610f4f56df4ba8147ea1f9ebcb60046c2a
Davide Masserut <d.masserut@gmail.com>
2016-05-18 22:23:48 +0200

    modified:   Scuola/2015_2016/Informatica/Sorgenti/ilTeatro/ilTeatro modified:   Scuola/2015_2016/Informatica/Sorgenti/ilTeatro/ilTeatro.cxx modified:   Scuola/2015_2016/Informatica/Sorgenti/tartaglia/tartaglia.cxx

--- Scuola/2015_2016/Informatica/Sorgenti/ilTeatro/ilTeatro.cxx
+++ Scuola/2015_2016/Informatica/Sorgenti/ilTeatro/ilTeatro.cxx
@@ -16,24 +16,34 @@ void InizPosti(int posti[NFILE][NPOLTRONE]){
 
 void Stampa(int posti[NFILE][NPOLTRONE], int nposti, int npostiocc){
 
-	cout<<" PALCO"<<endl<<" ";
-	for (int i=0;i<NPOLTRONE;i++){
+	cout<<"    PALCO"<<endl<<"    ";
+	for (int i=0;i<NPOLTRONE*2;i++){
 		cout<<"-";
 	}
-	cout<<endl<<" ";
+	cout<<endl<<"    ";
 
     for (int i=0;i<NPOLTRONE;i++){
-		cout<<i+1;
+		cout<<i+1<<"  ";
 	}
     cout<<endl;
 
     for (int i=0;i<NFILE;i++){
         cout<<i+1;
+		if(i+1<10){
+			cout<<"   ";
+		}
+		if((i+1<100)&&(i+1>9)){
+			cout<<"  ";
+		}
+		if((i+1<1000)&&(i+1>99)){
+			cout<<" ";
+		}
+
         for (int j=0;j<NPOLTRONE;j++){
             if (posti[i][j]==1){
-                cout<<"X";
+                cout<<"X  ";
             } else {
-                cout<<"-";
+                cout<<"-  ";
             }
         }
         cout<<endl;
@@ -67,6 +77,7 @@ void AssPosto(int posti[NFILE][NPOLTRONE], int nposti, int &npostiocc){
                 cout<<"Il posto è già occupato!"<<endl;
             }
         } while(posti[fila-1][poltrona-1]==1);
+
         posti[fila-1][poltrona-1]=1;
 		npostiocc++;
     }
@@ -74,11 +85,39 @@ void AssPosto(int posti[NFILE][NPOLTRONE], int nposti, int &npostiocc){
     return;
 }
 
+
+void AssPostCont(int posti[NFILE][NPOLTRONE], int nposti, int &npostiocc){
+	int posticont;
+	int contatore;
+	cout<<"Inserire il numero di posti contigui desiderati, il programma assegnerà i primi posti disponibili:"<<endl;
+	do{
+		cout<<"==> ";
+		cin>>posticont;
+		if((posticont<2)||(posticont>nposti)||(posticont+npostiocc>nposti)){
+			cout<<"Errore!"<<endl;
+		}
+	}while((posticont<2)||(posticont>nposti)||(posticont+npostiocc>nposti));
+
+	for(int i=0;i<NFILE;i++){
+		contatore=0;
+		for(int j=0;j<NPOLTRONE;i++){
+			if(posti[i][j]==1){
+				contatore++;
+			}
+			if(contatore==posticont){
+				break;
+			}
+		}
+	}
+
+	return;
+}
+
 int main(){
 	int posti[NFILE][NPOLTRONE];
 	int nposti=NFILE*NPOLTRONE;
 	int npostiocc=0;
-    bool scelta=true;
+    int  scelta;
 
 	cout<<"GESTORE POSTI TEATRO"<<endl;
 
@@ -86,10 +125,30 @@ int main(){
 
     do {
 		Stampa(posti,nposti,npostiocc);
-        AssPosto(posti,nposti,npostiocc);
+		cout<<"Premi 1 per assegnare un posto, 2 per assegnare più posti o 3 per liberare un posto"<<endl;
+		do{
+			cout<<"==> ";
+			cin>>scelta;
+		}while((scelta<1)||(scelta>3));
+		switch (scelta) {
+			case 1: cout<<"Assegnazione posto"<<endl;
+					AssPosto(posti,nposti,npostiocc);
+					break;
+			case 2: cout<<"Assegnazione posti contigui"<<endl;
+					AssPostCont(posti,nposti,npostiocc);
+					break;
+			case 3: cout<<"Eliminazione assegnazione posto"<<endl;
+
+					break;
+		}
+
 		Stampa(posti,nposti,npostiocc);
-        cout<<"Premi 0 per uscire dal programma o 1 per continuare: ";
-        cin>>scelta;
+        cout<<"Premi 0 per uscire dal programma o 1 per continuare: "<<endl;
+		do{
+			cout<<"==> ";
+			cin>>scelta;
+		}while((scelta!=0)||(scelta!=1));
+
     } while(scelta);
 
 	return 0;
--- Scuola/2015_2016/Informatica/Sorgenti/tartaglia/tartaglia.cxx
+++ Scuola/2015_2016/Informatica/Sorgenti/tartaglia/tartaglia.cxx
@@ -7,7 +7,7 @@ void Scelta(int &nrighe){
 		cout<<"\nInserisci numero righe\n==> ";
 		cin>>nrighe;
 	}while((nrighe<1)||(nrighe>DIM));
-	
+
 	return;
 }
 
@@ -16,28 +16,27 @@ void Inizializzazione(int vett1[], int vett2[]){
 		vett1[i]=0;
 		vett2[i]=0;
 	}
-	
+
 	return;
 }
 
 void Tartaglia(int vett1[], int vett2[], int nrighe){
 	int f;
-		
+
 	vett1[0]=1;
 	vett2[0]=1;
 	vett2[1]=1;
-	
-	
+
 	for(int i=0;i<nrighe;i++){
 		vett1[i]=1;
 		vett2[i]=1;
-		
+
 		f=nrighe-i-1;
 		do{
 			cout<<" ";
-			f--;			
+			f--;
 		}while(f>=0);
-		
+
 		for(int j=0;j<=i;j++){
 			if(i%2==0){
 				for(int k=j;k<j+1;k++){
@@ -61,10 +60,10 @@ void Tartaglia(int vett1[], int vett2[], int nrighe){
 
 int main(){
 	int vett1[DIM], vett2[DIM], nrighe;
-	
+
 	Scelta(nrighe);
 	//Inizializzazione(vett1,vett2);
 	Tartaglia(vett1,vett2,nrighe);
-	
+
 	return 1;
 }
