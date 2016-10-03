af91f48204280026349284da30eedd84c6e19898
mssdvd <d.masserut@gmail.com>
2016-10-03 21:28:07 +0200

    new file:   Scuola/2016_2017/Informatica/Sorgenti/borsa/.~lock.file e titoli di borsa.doc# modified:   Scuola/2016_2017/Informatica/Sorgenti/borsa/borsa.cxx modified:   Scuola/2016_2017/Informatica/Sorgenti/borsa/db.txt

--- Scuola/2016_2017/Informatica/Sorgenti/borsa/borsa.cxx
+++ Scuola/2016_2017/Informatica/Sorgenti/borsa/borsa.cxx
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <sstream>
 using namespace std;
 
 #define DIM 100
 
 struct data {
-	int giorno;
-	int mese;
-	int anno;
+	string giorno;
+	string mese;
+	string anno;
 };
 
 struct titolo {
 	string nome;
 	string codice;
-	struct data;
-	int quot;
+	struct data datidata;
+	string quot;
 };
 
 void ControlloFile(struct titolo titoli[DIM], fstream &file, char nomefile[128]) {
@@ -36,12 +37,7 @@ void ControlloFile(struct titolo titoli[DIM], fstream &file, char nomefile[128])
 	return;
 }
 
-void CaricoVettore(struct titolo titolimax[DIM], string riga) {
-
-
-}
-
-void LeggoFile(struct titolo titoli[DIM], struct titolo titolimax[DIM], fstream &file, char nomefile[128]) {
+void LeggoFile(struct titolo titoli[DIM], fstream &file, char nomefile[128], int &numline) {
 	file.open(nomefile, ios::in);
 	string riga;
 	int i = 0;
@@ -49,22 +45,67 @@ void LeggoFile(struct titolo titoli[DIM], struct titolo titolimax[DIM], fstream 
 	while(!file.eof()) {
 		getline(file, riga);
 		cout << riga << endl;
-
+		istringstream iss(riga);
+		getline(iss, titoli[i].nome, ' ');
+		getline(iss, titoli[i].codice, ' ');
+		getline(iss, titoli[i].datidata.giorno, '/');
+		getline(iss, titoli[i].datidata.mese, '/');
+		getline(iss, titoli[i].datidata.anno, ' ');
+		getline(iss, titoli[i].quot);
 		i++;
 	}
+	numline = i - 1;
 
 	return;
+}
+
+bool Presente(struct titolo titolomax[DIM], int numline2, string codice) {
+	for (int i = 0; i < numline2; i++) {
+		if (titolomax[i].codice == codice) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void CopioMax(struct titolo titoli[DIM], struct titolo titolimax[DIM], int &numline) {
+	string max;
+	int numline2 = 0;
+	for (int i = 0; i < numline; i++) {
+		max = titoli[i].quot;
+		for (int j = 0; j < numline; j++) {
+			if ((titoli[j].codice == titoli[i].codice) && (titoli[j].quot >= max)/* && Presente(titolimax, numline2, titoli[i].codice)*/) {
+				max = titoli[j].quot;
+				titolimax[i] = titoli[i];
+				numline2++;
+			}
+		}
+	}
+	numline = numline2;
+
+	return;
+}
+
+void Stampa(struct titolo titolimax[DIM], int numline) {
+	cout << "Stampa in corso..." << endl;
+	for (int i = 0; i < numline; i++) {
+		cout << titolimax[i].nome << " (" << titolimax[i].codice << ") " << titolimax[i].datidata.giorno << " " << titolimax[i].datidata.mese << " " << titolimax[i].datidata.anno << " " << titolimax[i].quot << endl;
+	}
 }
 
 int main (int argc, char const *argv[]) {
 	struct titolo titoli[DIM];
 	struct titolo titolimax[DIM];
 	fstream file;
+	int numline;
 	char nomefile[128];
 
 	cout << "Gestore titoli di borsa" << endl;
 	ControlloFile(titoli, file, nomefile);
-	LeggoFile(titoli, titolimax, file, nomefile);
+	LeggoFile(titoli, file, nomefile, numline);
+	CopioMax(titoli, titolimax, numline);
+	Stampa(titolimax, numline);
 
 	return 0;
 }
