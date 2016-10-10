630bfcb5f622f15d330d20564c2703ee7eae5d25
mssdvd <d.masserut@gmail.com>
2016-10-10 15:00:50 +0200

    modified:   borsa.cxx Untracked files:

--- Scuola/2016_2017/Informatica/Sorgenti/borsa/borsa.cxx
+++ Scuola/2016_2017/Informatica/Sorgenti/borsa/borsa.cxx
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <sstream>
+#include <string>
 using namespace std;
 
 #define DIM 100
@@ -15,11 +16,12 @@ struct data {
 struct titolo {
 	string nome;
 	string codice;
+	string data;
 	struct data datidata;
 	string quot;
 };
 
-void ControlloFile(struct titolo titoli[DIM], fstream &file, char nomefile[128]) {
+void ControlloFile(struct titolo t[DIM], fstream &file, char nomefile[128]) {
 
 	cout << "Inserisci il nome del file da cui prendere i dati: ";
 	cin >> nomefile;
@@ -37,75 +39,109 @@ void ControlloFile(struct titolo titoli[DIM], fstream &file, char nomefile[128])
 	return;
 }
 
-void LeggoFile(struct titolo titoli[DIM], fstream &file, char nomefile[128], int &numline) {
+void SoloMax(struct titolo t[DIM], int &numline) {
+
+	for (int i = 0; i < DIM; i++) {
+		for (int j = 0; j < DIM; j++) {
+			if (t[j].codice == t[i].codice) {
+				if (t[j].quot > t[i].quot) {
+					t[i].nome = "";
+				}
+			}
+		}
+		
+	}
+
+	return;
+}
+
+void LeggoFile(struct titolo t[DIM], fstream &file, char nomefile[128], int &numline) {
 	file.open(nomefile, ios::in);
-	string riga;
 	int i = 0;
+	string riga;
 
 	while(!file.eof()) {
 		getline(file, riga);
-		cout << riga << endl;
 		istringstream iss(riga);
-		getline(iss, titoli[i].nome, ' ');
-		getline(iss, titoli[i].codice, ' ');
-		getline(iss, titoli[i].datidata.giorno, '/');
-		getline(iss, titoli[i].datidata.mese, '/');
-		getline(iss, titoli[i].datidata.anno, ' ');
-		getline(iss, titoli[i].quot);
+		iss >> t[i].nome;
+		iss >> t[i].codice;
+		iss >> t[i].data;
+		iss >> t[i].quot;
 		i++;
+		numline++;
 	}
-	numline = i - 1;
-
 	return;
 }
 
-bool Presente(struct titolo titolomax[DIM], int numline2, string codice) {
-	for (int i = 0; i < numline2; i++) {
-		if (titolomax[i].codice == codice) {
-			return false;
-		}
-	}
+void Stampa(struct titolo t[DIM], int numline) {
+	string mese;
+	int nmese;
 
-	return true;
-}
-
-void CopioMax(struct titolo titoli[DIM], struct titolo titolimax[DIM], int &numline) {
-	string max;
-	int numline2 = 0;
-	for (int i = 0; i < numline; i++) {
-		max = titoli[i].quot;
-		for (int j = 0; j < numline; j++) {
-			if ((titoli[j].codice == titoli[i].codice) && (titoli[j].quot >= max)/* && Presente(titolimax, numline2, titoli[i].codice)*/) {
-				max = titoli[j].quot;
-				titolimax[i] = titoli[i];
-				numline2++;
-			}
-		}
-	}
-	numline = numline2;
-
-	return;
-}
-
-void Stampa(struct titolo titolimax[DIM], int numline) {
 	cout << "Stampa in corso..." << endl;
 	for (int i = 0; i < numline; i++) {
-		cout << titolimax[i].nome << " (" << titolimax[i].codice << ") " << titolimax[i].datidata.giorno << " " << titolimax[i].datidata.mese << " " << titolimax[i].datidata.anno << " " << titolimax[i].quot << endl;
+		if (t[i].nome != "") {
+			mese = t[i].data;
+			istringstream iss(t[i].data);
+			getline(iss, t[i].datidata.giorno, '/');
+			getline(iss, t[i].datidata.mese, '/');
+			getline(iss, t[i].datidata.anno);
+			stringstream(t[i].datidata.mese) >> nmese; //converte string -> int e copia nello struct
+			switch (nmese) {
+		 		case 1:
+		 			mese = "Gennaio";
+		 			break;
+			 	case 2:
+			 		mese = "Febbraio";
+			 		break;
+			 	case 3:
+			 		mese = "Marzo";
+			 		break;
+			 	case 4:
+			 		mese = "Aprile";
+			 		break;
+		 		case 5:
+			 		mese = "Maggio";
+		 			break;
+			 	case 6:
+			 		mese = "Giugno";
+			 		break;
+			 	case 7:
+			 		mese = "Luglio";
+			 		break;
+			 	case 8:
+			 		mese = "Agosto";
+			 		break;
+			 	case 9:
+			 		mese = "Settembre";
+		 			break;
+		 		case 10:
+		 			mese = "Ottobre";
+		 			break;
+		 		case 11:
+		 			mese = "Novembre";
+		 			break;
+		 		case 12:
+		 			mese = "Dicembre";
+		 			break;
+		 		default:
+		 			mese = "???";
+		 	}
+		 	cout << t[i].nome << " (" << t[i].codice << ") " << t[i].datidata.giorno << " " << mese << " " << t[i].datidata.anno << " " << t[i].quot << endl;
+		}	
 	}
 }
 
 int main (int argc, char const *argv[]) {
-	struct titolo titoli[DIM];
-	struct titolo titolimax[DIM];
+	struct titolo t[DIM];
 	fstream file;
-	int numline;
+	int numline = 0;
 	char nomefile[128];
 
 	cout << "Gestore titoli di borsa" << endl;
-	ControlloFile(titoli, file, nomefile);
-	LeggoFile(titoli, file, nomefile, numline);
-	CopioMax(titoli, titolimax, numline);
-	Stampa(titolimax, numline);
+	ControlloFile(t, file, nomefile);
+	LeggoFile(t, file, nomefile, numline);
+	SoloMax(t, numline);
+	Stampa(t, numline);
 
 	return 0;
 }
